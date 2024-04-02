e6cff38acc3b2118dff65f2b42db9a7fbb8e93e6
Rashed <rashedalamj@gmail.com>
2024-04-02 12:23:04 +0600

    improve ux

--- user/js/app.js
+++ user/js/app.js
@@ -1,9 +1,16 @@
+"use strict";
+
+// toggle sidebar
 const sidebar = document.querySelector("#sidebar");
-function toggle(){
+function toggle() {
   sidebar.classList.toggle("expand");
-  if(sidebar.classList.value){
-     document.querySelector(".main").style.width = "calc(100% - 260px)";
-  }else{
+  if (sidebar.classList.value) {
+    document.querySelector(".main").style.width = "calc(100% - 260px)";
+  } else {
     document.querySelector(".main").style.width = "calc(100% - 70px)";
   }
+}
+
+if (innerWidth > 1100) {
+  toggle();
 }
